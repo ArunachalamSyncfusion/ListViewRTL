26a18fc388356c8f1c034026e87163d3f0b3073c
Arunachalam3641 <arunachalam.sf3641@gmail.com>
2024-05-14 15:22:58 +0530

    Added the sample.

---
+++ ListViewRTL/ListViewRTL/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ListViewRTL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>();
+            list.Add("Home");
+            list.Add("Profile");
+            list.Add("Inbox");
+            list.Add("Outbox");
+            list.Add("Sent");
+            list.Add("Draft");
+            listView.ItemsSource = list;
+        }
+    }
+
+}
